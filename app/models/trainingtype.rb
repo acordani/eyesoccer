e1a7ced2fb1a4c731f38312d362f74df37700402
ac <alcordani@gmail.com>
2017-01-05 21:41:58 +0100

    Model

--- app/models/trainingtype.rb
+++ app/models/trainingtype.rb
@@ -1,3 +1,4 @@
 class Trainingtype < ActiveRecord::Base
   belongs_to :user
+  has_many :events, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,6 @@ class User < ActiveRecord::Base
 
 
   has_many :trainingtypes, dependent: :destroy
+  has_many :events, dependent: :destroy
 
 end
